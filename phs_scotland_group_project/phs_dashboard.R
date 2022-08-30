9eb636b983aafb10ec11c5c884c0913e32cdf6b1
lloydlombardi <108779828+lloydlombardi@users.noreply.github.com>
2022-08-30 12:07:57 +0100

    first tab

--- phs_scotland_group_project/phs_dashboard.R
+++ phs_scotland_group_project/phs_dashboard.R
@@ -3,19 +3,119 @@
 
 library(tidyverse)
 library(shiny)
-
-
+library(bslib)
+library(shinydashboard)
+library(shinyWidgets)
+library(lubridate)
 
 # data wrangling ----------------------------------------------------------
 
+waiting_times <- read_csv("raw_data/non_covid_raw_data/monthly_ae_waitingtimes_202206.csv") %>% janitor::clean_names()
 
+waiting_times <- waiting_times %>% 
+  mutate(date = ym(month),
+         year = year(date))
+
+all_years <- waiting_times %>% 
+  distinct(year) %>% 
+  arrange(year) %>% 
+  pull()
 
 
 
 # ui ----------------------------------------------------------------------
 
-ui <- fluidPage(
+ui <- navbarPage(
   
+  theme = bs_theme(bootswatch = "united"),
+  
+  title = "Public Health Scotland Dashboard Project",
+  
+  position = "static-top",
+  
+  fluid = TRUE,
+  
+  tabPanel("A&E Overview",
+           
+           fluidRow(
+             
+             column(6,
+                    infoBox(value = "96%", title =tags$h3("Waiting Time"), subtitle = NULL, icon = icon(name = "heart", lib = "font-awesome"))     
+             ),
+             
+             column(6,
+                    selectInput(inputId = "year_choice",
+                                label = tags$h3("Year"),
+                                choices = all_years)
+             )
+           ),
+           
+           fluidRow(
+             
+             box(
+               title = "A&E LINE GRAPH",
+               status = "primary",
+               plotOutput("ae_wait_times")
+             ),
+             
+             box(
+               title = "SCOTLAND MAP",
+               status = "warning",
+               plotOutput("ae_map")
+             )
+           )
+           
+           
+  ),
+  
+  
+  tabPanel("Winter Crisis",
+           
+           
+           
+           ),
+  
+  
+  tabPanel("Impact of COVID-19",
+           
+           
+           
+           ),
+  
+  
+  tabPanel("Age Group",
+           
+           
+           
+           ),
+  
+  
+  tabPanel("Sex",
+           
+           
+           
+           ),
+  
+  
+  tabPanel("SIMD",
+           
+           
+           
+           ),
+  
+  
+  tabPanel("Geospatial Maps",
+           
+           
+           
+           ),
+  
+  
+  tabPanel("Data",
+           
+           
+           
+           )
   
   
 )
