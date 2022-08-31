a4e9082bb334725678f28b16b237abd057843dfe
lloydlombardi <108779828+lloydlombardi@users.noreply.github.com>
2022-08-31 08:42:03 +0100

    updated cleaning script

--- phs_scotland_group_project/lloyd_cleaning_script.R
+++ phs_scotland_group_project/lloyd_cleaning_script.R
@@ -17,7 +17,19 @@ waiting_times <- waiting_times %>%
   mutate(prop_other_speciality = (discharge_destination_other_specialty/number_of_attendances_aggregate)) %>% 
   mutate(prop_residence = (discharge_destination_residence/number_of_attendances_aggregate)) %>% 
   mutate(prop_transfer = (discharge_destination_transfer/number_of_attendances_aggregate)) %>% 
-  mutate(prop_unknown = (discharge_destination_unknown/number_of_attendances_aggregate)) 
+  mutate(prop_unknown = (discharge_destination_unknown/number_of_attendances_aggregate))
+
+waiting_times <- waiting_times %>% 
+  pivot_longer(cols = prop_admission_to_same:prop_unknown, names_to = "discharge_destination", values_to = "discharge_proportion")
+
+waiting_times <- waiting_times %>% 
+  mutate(discharge_destination = case_when(
+    str_detect(discharge_destination, "prop_admission_to_same") ~ "Admission to Same Facility",
+    str_detect(discharge_destination, "prop_other_speciality") ~ "Transfer to Other Facility",
+    str_detect(discharge_destination, "prop_residence") ~ "Transfer to Residence",
+    str_detect(discharge_destination, "prop_transfer") ~ "Transfer to Private Facility",
+    str_detect(discharge_destination, "prop_unknown") ~ "Unknown"
+  ))
 
 
 write_csv(waiting_times, "clean_data/waiting_times.csv")
